061ff0d45aadb37be6acce5ce5c0466a1855f2f2
Philip Owolabi <philipowolabi79@gmail.com>
2023-12-29 14:42:21 +0100

    Setup fonts and type faces

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="neue-ultrabold">Hello</h1>
+      <p className="gray-white-color">World</p>
+    </div>
+  );
+};
+
+export default Navbar;
