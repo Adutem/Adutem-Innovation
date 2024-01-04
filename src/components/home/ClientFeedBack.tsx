b4250800dec6ad3572fde9cd53fe6903809d15db
Philip Owolabi <philipowolabi79@gmail.com>
2024-01-04 22:47:58 +0100

    Add slider arrow icon to review page

--- src/components/home/ClientFeedBack.tsx
+++ src/components/home/ClientFeedBack.tsx
@@ -1,56 +1,10 @@
 import styled from "styled-components";
 import ClientFeedBackCard from "./ClientFeedBackCard";
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import feedbacks from "../../data/feedback";
 
 const ClientFeedBack = () => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [justRendered, setJustRendered] = useState(true);
-  const [direction, setDirection] = useState("");
-  const [currentData, setCurrentData] = useState(() => feedbacks[activeIndex]);
-  const infoContainerRef = useRef<HTMLElement | undefined>(undefined);
-  const whatTheySayRef = useRef<HTMLElement | undefined>(undefined);
-  const clientNameRef = useRef<HTMLElement | undefined>(undefined);
-  const clientRoleRef = useRef<HTMLElement | undefined>(undefined);
-  const thumbnailImageRef = useRef<HTMLElement | undefined>(undefined);
-
-  useEffect(() => {
-    console.log("Called useEffect", activeIndex);
-    if (justRendered) {
-      setJustRendered(false);
-      return;
-    }
-    if (infoContainerRef.current) {
-      let currents = [
-        infoContainerRef.current,
-        clientNameRef.current!,
-        clientRoleRef.current!,
-        thumbnailImageRef.current!,
-        whatTheySayRef.current!,
-      ];
-      currents.forEach((el) => {
-        if (el) {
-          el.classList.add("fade-out");
-        }
-      });
-
-      let displayTmo = setTimeout(() => {
-        currents.forEach((el) => {
-          if (el) {
-            el.classList.remove("fade-out");
-          }
-        });
-        setCurrentData(feedbacks[activeIndex]);
-        clearTimeout(displayTmo);
-      }, 300);
-      // infoContainerRef.current.style.display = "none";
-
-      // let displayTmo = setTimeout(() => {
-      //   infoContainerRef.current.style.display = "block";
-      //   clearTimeout(displayTmo);
-      // }, 100);
-    }
-  }, [activeIndex]);
 
   return (
     <Container className="bg-[hsl(240,12%,18%)] py-24 w-full">
@@ -58,14 +12,27 @@ const ClientFeedBack = () => {
         <p className="text-blue-700 text-[2rem]">We love our clients</p>
         <p className="text-2xl mb-4">And clients love to work with us</p>
         <ClientFeedBackCard activeIndex={activeIndex} />
-        <button
-          onClick={() =>
-            setActiveIndex((prev) => (prev >= 2 ? 0 : Math.min(prev + 1, 2)))
-          }
-          className="p-6 bg-blue-700"
-        >
-          Click to move index
-        </button>
+        <div className="flex items-center mt-12">
+          <div className="flex-1"> </div>
+          <div className="flex items-center gap-4">
+            <i
+              className="fi fi-sr-arrow-alt-left flex text-white hover:text-blue-700 transition-colors text-3xl cursor-pointer"
+              onClick={() =>
+                setActiveIndex((prev) =>
+                  prev <= 0 ? 2 : Math.max(prev - 1, 0)
+                )
+              }
+            ></i>
+            <i
+              className="fi fi-sr-arrow-alt-right flex text-white hover:text-blue-700 transition-colors text-3xl cursor-pointer"
+              onClick={() =>
+                setActiveIndex((prev) =>
+                  prev >= 2 ? 0 : Math.min(prev + 1, 2)
+                )
+              }
+            ></i>
+          </div>
+        </div>
       </div>
     </Container>
   );
