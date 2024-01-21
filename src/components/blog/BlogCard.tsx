8dc9ebedd89c479a22c8998de83a99e80dfb2a9e
Philip Owolabi <philipowolabi79@gmail.com>
2024-01-21 14:48:31 +0100

    Update blog page layout

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { BlogPost } from "../../data/blog";
 import styled from "styled-components";
 
-interface BlogCardProps extends BlogPost {}
+interface BlogCardProps extends BlogPost {
+  index: number;
+}
 
 const BlogCard = ({
   thumbNail,
@@ -10,29 +12,45 @@ const BlogCard = ({
   publicationDate,
   title,
   shortDescription,
+  index,
 }: BlogCardProps) => {
   return (
-    <Container>
-      <div>
-        <div className="w-full rounded-lg overflow-hidden h-[200px] sm:h-[280px] md:h-[300px]">
-          <img src={thumbNail} className="w-full h-full object-cover" />
+    <Container className="">
+      <div className="md:grid grid-cols-5 gap-4 md:gap-6 lg:gap-10 items-center">
+        <div
+          className={`md:col-span-2 lg:col-span-3 h-auto ${
+            index % 2 === 0 ? "order-1" : "order-2"
+          }`}
+        >
+          <div className="w-full rounded-lg overflow-hidden h-[200px] sm:h-[280px] md:h-[300px] lg:h-[350px]">
+            <img src={thumbNail} className="w-full h-full object-cover" />
+          </div>
+        </div>
+        <div
+          className={`md:col-span-3 lg:col-span-2 ${
+            index % 2 === 0 ? "order-2" : "order-1"
+          }`}
+        >
+          <div className="flex gap-4 my-2 mt-4">
+            <p className="uppercase text-xs faded">{tag}</p>
+            <p className="uppercase text-xs faded">
+              {new Date(publicationDate).toDateString()}
+            </p>
+          </div>
+          <h2
+            className="text-xl text-[var(--base-color)] md:leading-[1.1] md:w-10/12"
+            style={{ fontSize: "clamp(1.45rem, 4.5vw, 2.4rem)" }}
+          >
+            {title}
+          </h2>
+          <p
+            className="mt-4"
+            style={{ fontSize: "clamp(0.8rem, 4.5vw, 18px)" }}
+          >
+            {shortDescription}
+          </p>
         </div>
       </div>
-      <div className="flex gap-4 my-2 mt-4">
-        <p className="uppercase text-xs faded">{tag}</p>
-        <p className="uppercase text-xs faded">
-          {new Date(publicationDate).toDateString()}
-        </p>
-      </div>
-      <h2
-        className="text-xl text-[var(--base-color)]"
-        style={{ fontSize: "clamp(1.45rem, 4.5vw, 2rem)" }}
-      >
-        {title}
-      </h2>
-      <p className="mt-4" style={{ fontSize: "clamp(0.8rem, 4v.5w, 18px)" }}>
-        {shortDescription}
-      </p>
     </Container>
   );
 };
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -13,7 +13,7 @@ const CustomDivi = styled(Divi)`
 document.documentElement.style.background = "var(--black)";
 const Blog = () => {
   return (
-    <CustomDivi className="text-white">
+    <CustomDivi className="text-white w-[100%] md:w-[90%] lg:max-w-[1024px] md:mx-auto lg:w-11/12">
       <div className="relative bg-gray-800 px-8 flex items-center rounded-full gap-6">
         <i className="fi fi-rr-search flex text-xl"></i>
         <input
@@ -22,9 +22,9 @@ const Blog = () => {
         />
       </div>
       <div className="my-8">
-        <div className="grid grid-cols-1 gap-y-12">
-          {blogPosts.map((blogPost) => (
-            <BlogCard {...blogPost} />
+        <div className="grid grid-cols-1 gap-y-12 md:gap-y-16 lg:gap-y-24">
+          {blogPosts.map((blogPost, i) => (
+            <BlogCard {...blogPost} index={i} />
           ))}
         </div>
       </div>
