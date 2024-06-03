4f4fc191a768c1c1bc2769d9d753d29eaba1f73b
philipthedeveloper <88097351+philipthedeveloper@users.noreply.github.com>
2024-06-03 13:30:11 +0100

    Update gitignore to omit env and update config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 const config = {
   // API_URL: "https://streaks-backend-c93c.onrender.com/streaks/api/v1/",
   // API_URL: "https://adutem-innovation-backend.onrender.com/adutem/api/v1/",
-  API_URL: "http://localhost:5000/adutem/api/v1/",
+  // API_URL: "http://localhost:5000/adutem/api/v1/",
+  API_URL: import.meta.env.VITE_API_URL,
   FIRE_BASE: {
     //   API_KEY: process.env.REACT_APP_APIKEY,
     //   AUTH_DOMAIN: process.env.REACT_APP_AUTHDOMAIN,
